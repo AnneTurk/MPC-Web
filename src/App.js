d9d071984e72a09d977c5f1bb54be262d83573d4
AnneTurk <anne@semantica.nl>
2019-06-03 12:02:54 +0200

    react router implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { render } from "react-dom";
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const Home = () => <div> Home </div>;
+const Shopping = () => <div>Shopping</div>;
+const Product = ({match}) => <div>Product Page</div>;
+const Cart = () => <div>Cart Page</div>;
+const Contact = () => <div>Contact Page</div>;
+const About = () => <div>About page</div> 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <BrowserRouter>
+      <header className="">
       </header>
-    </div>
+    <div className="App">
+    <div>
+      <ul>
+        <li>
+          <Link to="/#/">Home</Link>
+        </li>
+        <li>
+          <Link to="/#/shopping">Shopping Page</Link>
+        </li>
+        <li>
+          <Link to="/#/product">Product Page</Link>{" "}
+        </li>
+        <li>
+          <Link to="/#/cart">Cart Page</Link>
+        </li>
+        <li>
+          <Link to="/#/contact">Contact Page</Link>
+        </li>
+        <li>
+          <Link to="/#/about">About Page</Link>
+        </li>
+      </ul>
+      <hr/>
+      <Route path="/#/" component={Home}/> {/*-rubric13 */}
+      <Route path="/#/shopping" component={Shopping} />{/*-rubric34 */}
+      <Route path="/#/product" component={Product} />{/*-rubric46 */}
+      <Route path="/#/cart" component={Cart} />{/*-rubric56 */}
+      <Route path="/#/contact" component={Contact} />{/*-rubric62 */}
+      <Route path="/#/about" component={About} />{/*-rubric64 */}
+      </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
